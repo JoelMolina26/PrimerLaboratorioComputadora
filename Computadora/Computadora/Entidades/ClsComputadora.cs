fe9e6a8dc391bcc762b737cccba367caf0d587ce
MarvinRivas07 <88003076+MarvinRivas07@users.noreply.github.com>
2021-08-16 16:04:19 -0600

    Encapsulamiento

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -8,9 +8,28 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
-        private String color;
-        private String marca;
+        
         private String motherboard;
         private int ram;
+        private String discoduro;
+        private int  procesador;
+        private String tarjetadevideo;
+        private String teclado;
+        private String mouse;
+        private String marca;
+        private String color;
+        private String modelo;
+
+
+        public string Motherboard { get => motherboard; set => motherboard = value; }
+        public int Ram { get => ram; set => ram = value; }
+        public string Discoduro { get => discoduro; set => discoduro = value; }
+        public int Procesador { get => procesador; set => procesador = value; }
+        public string Tarjetadevideo { get => tarjetadevideo; set => tarjetadevideo = value; }
+        public string Teclado { get => teclado; set => teclado = value; }
+        public string Mouse { get => mouse; set => mouse = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Color { get => color; set => color = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
     }
 }
