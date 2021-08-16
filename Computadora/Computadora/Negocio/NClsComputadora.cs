8393a10a792b1c4d41f28556b7ecfb6a72240047
JoelJR26 <88001656+JoelJR26@users.noreply.github.com>
2021-08-16 16:39:27 -0600

    Negocio 1

--- Computadora/Computadora/Negocio/NClsComputadora.cs
+++ Computadora/Computadora/Negocio/NClsComputadora.cs
@@ -12,9 +12,24 @@ namespace Computadora.Negocio
         public String Encender(ClsComputadora computadora)
         {
 
-            return " La computadora con memoria ram de" + computadora.Ram + "procesador" + computadora.Procesador + "Esta Encendida";
+            return " La computadora con memoria Ram de" + computadora.Ram + "procesador" + computadora.Procesador + "Encender computadora";
+
+        }
+
+        public String Apagar(ClsComputadora computadora)
+        {
+            return "La computadora con memoria Ram" + computadora.Ram + "procesador" + computadora.Procesador + "Apagar computadora";
+
+        }
+        public String Reiniciar(ClsComputadora computadora)
+        {
+            return "La computadora con memoria Ram" + computadora.Ram + "procesador" + computadora.Procesador + "Reiniciar computadora";
 
 
+        }
+        public String Suspender(ClsComputadora computadora)
+        {
+            return "La computadora con memoria Ram" + computadora.Ram + "procesador" + computadora.Procesador + "Suspender computadora";
         }
     }
 }
